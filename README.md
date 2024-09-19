2d99262547ed4da6a894727a847e9a1c3414dde6
cheyao <94928179+cheyao@users.noreply.github.com>
2024-09-19 20:17:50 +0200

    Start of game

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Bootsect breakout!
 
 Lets see how far my asm skills gets me :D
+
+Good old resources I had in my bookmarks:
+
+- x64 inst table: https://c9x.me/x86/
+- bootloader interrupts: https://www.ctyme.com/intr/int.htm
+- OSDev wiki: https://wiki.osdev.org/
--- breakout.asm
+++ breakout.asm
@@ -2,11 +2,33 @@ org 0x7C00
 bits 16
 cpu 8086
 
-	mov ax, 0
+SCREEN_WIDTH	equ 320
+SCREEN_HEIGHT	equ 200
+
+	; Init routine
+	xor ax, ax
 	mov ss, ax
 	mov ds, ax
-	mov es, ax
+
+	mov ax, 0xA000
+	mov es, ax ; Video memory
+
+	; Set video mode
+	mov ax, 0x0013
+	int 0x10
+
+	; Stack
 	mov sp, 0x7C00
+
+	; Clear screen
+	mov al, 0x00
+	mov cx, SCREEN_WIDTH * SCREEN_HEIGHT
+	xor di, di
+	rep stosb
 
 loop:
 	jmp loop
+
+; Boot stuff
+times 510 - ($-$$) db 0
+dw 0xaa55
